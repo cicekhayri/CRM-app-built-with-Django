bec118383221fa0654e23c6e5d16dc6c58e1416b
Hayri Cicek <hello@hayricicek.se>
2016-07-26 22:19:28 +0200

    created the subscriber view

--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+from .forms import SubscriberForm
+
+
+def subscriber_new(request, template='subscriber/subscriber_new.html'):
+    if request.method == 'POST':
+        form = SubscriberForm(request.POST)
+        if form.is_valid():
+            # unpack form values
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            email = form.cleaned_data['email']
+            # create the user record
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            # create Subscriber Record
+            # Process payment (via Stripe)
+            # auto login the user
+            return HttpResponseRedirect('/success/')
+    else:
+        form = SubscriberForm()
+
+    return render(request, template, {'form':form})
