480e958e9ef3c0f9698a7af9388a0c32a7c9f9bc
Hayri Cicek <hello@hayricicek.se>
2016-07-26 22:55:06 +0200

    created the subscriber model

--- crmapp/subscribers/models.py
+++ crmapp/subscribers/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    user_rec = models.ForeignKey(User)
+    address_one = models.CharField(max_length=100)
+    address_two = models.CharField(max_length=100, blank=True)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    stripe_id = models.CharField(max_length=30, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'subscribers'
+
+    def __str__(self):
+        return "%s's Subscription Info" % self.user_rec
