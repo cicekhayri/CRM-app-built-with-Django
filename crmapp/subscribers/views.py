e9ec0afe750c48e12377542828a7e194e5d86396
Hayri Cicek <hello@hayricicek.se>
2016-07-27 08:55:07 +0200

    updated the view

--- crmapp/subscribers/views.py
+++ crmapp/subscribers/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 
-
 from .forms import SubscriberForm
+from .models import Subscriber
 
 
 def subscriber_new(request, template='subscribers/subscriber_new.html'):
@@ -14,11 +14,21 @@ def subscriber_new(request, template='subscribers/subscriber_new.html'):
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
             email = form.cleaned_data['email']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
             # create the user record
-            user = User(username=username, email=email)
+            user = User(username=username, email=email,
+                        first_name=first_name, last_name=last_name)
             user.set_password(password)
             user.save()
             # create Subscriber Record
+            address_one = form.cleaned_data['address_one']
+            address_two = form.cleaned_data['address_two']
+            city = form.cleaned_data['city']
+            state = form.cleaned_data['state']
+            sub = Subscriber(address_one=address_one, address_two=address_two,
+                            city=city, state=state, user_rec=user)
+            sub.save()
             # Process payment (via Stripe)
             # auto login the user
             return HttpResponseRedirect('/success/')
