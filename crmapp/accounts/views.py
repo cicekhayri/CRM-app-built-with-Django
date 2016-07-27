c2653fcd273646b2d703c502906c31506eab31b8
Hayri Cicek <hello@hayricicek.se>
2016-07-27 13:53:28 +0200

    created the new communication view

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -7,6 +7,7 @@ from django.utils.decorators import method_decorator
 
 from crmapp.contacts.models import Contact
 from crmapp.communications.models import Communication
+from crmapp.communications.forms import CommunicationForm
 
 from .models import Account
 from .forms import AccountForm
@@ -60,10 +61,13 @@ def account_detail(request, uuid):
     communications = Communication.objects.filter(
         account=account).order_by('-created_on')
 
+    form = CommunicationForm()
+
     variables = {
         'account': account,
         'contacts': contacts,
         'communications': communications,
+        'form': form
     }
 
     return render(request, 'accounts/account_detail.html', variables)
--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -1,11 +1,44 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseForbidden
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from .models import Communication
+from .forms import CommunicationForm
 
 
-@login_required
+@login_required()
+def comm_cru(request):
+    if request.POST:
+        form = CommunicationForm(request.POST)
+        if form.is_valid():
+            # make sure the user owns the account
+            account = form.cleaned_data['account']
+            if account.owner != request.user:
+                return HttpResponseForbidden()
+            # save the data
+            comm = form.save(commit=False)
+            comm.owner = request.user
+            comm.save()
+            # return the user to the account detail view
+            reverse_url = reverse(
+                'crmapp.accounts.views.account_detail',
+                args=(account.uuid,)
+            )
+            return HttpResponseRedirect(reverse_url)
+    else:
+        form = CommunicationForm()
+
+    variables = {
+        'form': form,
+    }
+
+    template = 'communications/comm_cru.html'
+
+    return render(request, template, variables)
+
+
+@login_required()
 def comm_detail(request, uuid):
 
     comm = Communication.objects.get(uuid=uuid)
