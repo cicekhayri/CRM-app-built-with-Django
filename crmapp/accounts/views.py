f7b1d53be440c918dd4f1f620c1fef8bb5cc1c6a
Hayri Cicek <hello@hayricicek.se>
2016-07-27 11:17:35 +0200

    added pagination to account list

--- crmapp/accounts/views.py
+++ crmapp/accounts/views.py
@@ -8,6 +8,7 @@ from .models import Account
 
 class AccountList(ListView):
     model = Account
+    paginate_by = 12
     template_name = 'accounts/account_list.html'
     context_object_name = 'accounts'
 
