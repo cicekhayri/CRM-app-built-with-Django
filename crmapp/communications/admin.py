ae497924f5473e47f387bbbecfe89100d35d827c
Hayri Cicek <hello@hayricicek.se>
2016-07-27 13:29:35 +0200

    created the communications model

--- crmapp/communications/admin.py
+++ crmapp/communications/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Communication
+
+
+class CommunicationAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'uuid')
+
+admin.site.register(Communication, CommunicationAdmin)
