3a34b4c65673b1289d23f38232f8199a62cf5c18
Hayri Cicek <hello@hayricicek.se>
2016-07-27 13:05:35 +0200

    activated AJAX for contact edit/new features

--- crmapp/static/js/app.js
+++ crmapp/static/js/app.js
@@ -59,5 +59,43 @@ $(document).ready(function() {
         e.preventDefault();
         $('#gi-container').load($(this).attr('href'));
     });
-    
+
+    // Contact - User AJAX to get the Contact Add Form
+    $('#cd-container').delegate('#new-contact', 'click', function(e) {
+        e.preventDefault():
+        $.get($(this).attr('href'), function(data) {
+            $('#cd-body').append(data);
+            $('#new-contact').hide();
+        });
+    });
+
+    // Contact - Use AJAX to get the Contact Edit form
+    $('#cd-container').delegate('.edit-contact', 'click', function(e) {
+        e.preventDefault();
+        var that = $(this);
+        $.get($(this).attr('href'), function(data) {
+            $('#new-contact').hide();
+            that.parent().parent().remove();
+            $('#cd-body').append(data);
+        })
+    });
+
+    // Contact - Use AJAX to save the Contact Add Form
+    $('#cd-container').delegate('#contact-form', 'submit', function(e) {
+        e.preventDefault();
+        var form = $('#contact-form');
+        var url = form.attr('action');
+        $.post(url, form.serialize(), function(data) {
+            if ($(data).find('.errorlist').html()) {
+                // if the contact form is returned we know there are errors
+                $('#new-contact').hide();
+                $('#cd-body').append(data);
+            } else {
+                // Otherwise insert the row into the table
+                $('#cd-table tr:last').after(data);
+                $('#new-contact').show();
+            }
+        });
+        $(this).remove();
+    });
 });
