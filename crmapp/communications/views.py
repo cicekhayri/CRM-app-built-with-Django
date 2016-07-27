74408d96a3663d199ebe8e6190d12cbda7c5ef47
Hayri Cicek <hello@hayricicek.se>
2016-07-27 14:09:55 +0200

    added AJAX to communication

--- crmapp/communications/views.py
+++ crmapp/communications/views.py
@@ -32,11 +32,17 @@ def comm_cru(request, uuid=None, account=None):
             comm.owner = request.user
             comm.save()
             # return the user to the account detail view
-            reverse_url = reverse(
-                'crmapp.accounts.views.account_detail',
-                args=(account.uuid,)
-            )
-            return HttpResponseRedirect(reverse_url)
+            if request.is_ajax():
+                return render(request,
+                                'communications/comm_item_view.html',
+                                {'comm': commm, 'account': account}
+                )
+            else:
+                reverse_url = reverse(
+                    'crmapp.accounts.views.account_detail',
+                    args=(account.uuid,)
+                )
+                return HttpResponseRedirect(reverse_url)
         else:
             # if the form isn't valid, still fetch the account so it can be passed to the template
             account = form.cleaned_data['account']
@@ -54,7 +60,10 @@ def comm_cru(request, uuid=None, account=None):
         'account': account
     }
 
-    template = 'communications/comm_cru.html'
+    if request.is_ajax():
+        template = 'communication/comm_item_form.html'
+    else:
+        template = 'communications/comm_cru.html'
 
     return render(request, template, variables)
 
